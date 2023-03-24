72890a6b8ef3b58171cc95e0c5459513af0a2ea2
Alexander I <apcrf@mail.ru>
2023-03-24 15:40:02 +0200

    menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -70,6 +70,9 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('post-list')" :active="request()->routeIs('post-list')">
+                Blog Posts
+            </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
